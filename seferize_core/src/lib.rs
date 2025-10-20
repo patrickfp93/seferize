de1949c71629bfc740ca5f83ee2880d64a46b2db
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2025-10-20 19:06:34 -0300

    fix expose_for_test

--- seferize_core/src/lib.rs
+++ seferize_core/src/lib.rs
@@ -12,7 +12,7 @@ const EXPOSE_TEST_PREFIX : &'static str = "testable_";
 
 pub use crate::parameters::Parameters;
 
-use crate::extractor::Extractor;
+use crate::{extractor::Extractor, util::generate_call};
 #[allow(unused)]
 pub(crate) use crate::samples::{
     extract_into::{enumerate::*, implementation::*, structs::*},
@@ -25,7 +25,7 @@ use crate::{
 };
 use proc_macro2::TokenStream;
 use quote::{ToTokens, quote};
-use syn::{parse_quote, Item, ItemFn};
+use syn::{parse_quote, parse_str, Ident, Item, ItemFn};
 
 pub fn stringify(parameters: Parameters, item: TokenStream) -> TokenStream {
     // Lê o item e o atributo (se existir)
@@ -54,16 +54,22 @@ pub fn stringify(parameters: Parameters, item: TokenStream) -> TokenStream {
 
 pub fn expose_for_tests(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse da função original
-    let input_fn : ItemFn = parse_quote!(#item);
-    let fn_name = format!("{}{}",EXPOSE_TEST_PREFIX,input_fn.sig.ident);
+    let original_fn : ItemFn = parse_quote!(#item);
+    let fn_name : Ident = parse_str(&format!("{}{}",EXPOSE_TEST_PREFIX,original_fn.sig.ident)).unwrap();
+
+    let mut exposed_fn = original_fn.clone();
+    exposed_fn.sig.ident = fn_name;
+    let called_fn = generate_call(&original_fn);
+    exposed_fn.block = parse_quote!{
+        #called_fn
+    };
+    exposed_fn.vis = parse_quote!(pub);
 
     // Gera o nome da função de teste (pode ser igual, com pub e cfg)
     quote! {
-        #input_fn
-
         #[cfg(test)]
-        pub fn #fn_name(field: &Field, index: usize) -> ItemFn {
-            #fn_name(field, index)
-        }
+        #exposed_fn        
+        #original_fn
     }
 }
+
--- seferize_core/src/util.rs
+++ seferize_core/src/util.rs
@@ -1,11 +1,36 @@
 use proc_macro2::TokenStream;
 use quote::quote;
-use syn::parse_str;
+use syn::{parse_str, ItemFn, Pat};
 
 
 pub fn build_const(name: &str, content: &str) -> TokenStream {
     let name: TokenStream = parse_str(name).unwrap();
     quote! {
         pub const #name: &'static str = #content;
+    }
+}
+
+pub fn generate_call(fn_item: &ItemFn) -> TokenStream {
+    // Extrai os nomes dos argumentos
+    let arg_names: Vec<TokenStream> = fn_item.sig.inputs.iter().map(|arg| {
+        match arg {
+            syn::FnArg::Receiver(_) => quote! { self },   // &self ou self
+            syn::FnArg::Typed(pat_type) => {
+                match *pat_type.pat.clone() {
+                    Pat::Ident(ref ident) => {
+                        let name = &ident.ident;
+                        quote! { #name }
+                    },
+                    _ => quote! { _ } // caso não seja simples
+                }
+            }
+        }
+    }).collect();
+
+    let fn_name = &fn_item.sig.ident;
+
+    // Cria a chamada
+    quote! {
+        #fn_name(#(#arg_names),*)
     }
 }
