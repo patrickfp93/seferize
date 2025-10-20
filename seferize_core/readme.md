efb9e47ff71798ab9ac5270443833317d02550f3
Patrick Fernandes Peçanha <patrickfp93@gmail.com>
2025-10-20 18:54:12 -0300

    add suport for expose_for_tests

--- seferize_core/readme.md
+++ seferize_core/readme.md
@@ -2,4 +2,4 @@
 
 "The engine behind Seferize procedural macros." ⚙️
 
-Seferize Core is a Rust library that powers the seferize procedural macro by parsing Rust items and generating their string representations. It supports attributes like #[ignore] and #[stringify], handles structs, enums, traits, impls, fields, variants, and methods, and is fully testable and safe, providing a reliable foundation for code introspection and compile-time code analysis.
+Seferize Core is a Rust library that powers the seferize procedural macro by parsing Rust items and generating their string representations. It supports attributes like #[ignore],#[expose_for_tests] and #[stringify], handles structs, enums, traits, impls, fields, variants, and methods, and is fully testable and safe, providing a reliable foundation for code introspection and compile-time code analysis.
--- seferize_core/src/lib.rs
+++ seferize_core/src/lib.rs
@@ -8,6 +8,7 @@ mod util;
 static OCCURRENCES: [&'static str; 2] = ["seferize::stringify", "stringify"];
 static IGNORE_OCCURRENCES: [&'static str; 2] = ["seferize::ignore", "ignore"];
 const CONST_DEFAULT_PREFIX: &'static str = "CODE_";
+const EXPOSE_TEST_PREFIX : &'static str = "testable_";
 
 pub use crate::parameters::Parameters;
 
@@ -24,7 +25,7 @@ use crate::{
 };
 use proc_macro2::TokenStream;
 use quote::{ToTokens, quote};
-use syn::{Item, parse_quote};
+use syn::{parse_quote, Item, ItemFn};
 
 pub fn stringify(parameters: Parameters, item: TokenStream) -> TokenStream {
     // Lê o item e o atributo (se existir)
@@ -48,5 +49,21 @@ pub fn stringify(parameters: Parameters, item: TokenStream) -> TokenStream {
         #(#consts)*
 
         #original
+    }
+}
+
+pub fn expose_for_tests(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse da função original
+    let input_fn : ItemFn = parse_quote!(#item);
+    let fn_name = format!("{}{}",EXPOSE_TEST_PREFIX,input_fn.sig.ident);
+
+    // Gera o nome da função de teste (pode ser igual, com pub e cfg)
+    quote! {
+        #input_fn
+
+        #[cfg(test)]
+        pub fn #fn_name(field: &Field, index: usize) -> ItemFn {
+            #fn_name(field, index)
+        }
     }
 }
